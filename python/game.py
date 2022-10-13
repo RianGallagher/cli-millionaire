3513f1fc1d08be923869f84b7e10125fa9df87d3
Rian Gallagher <rianogallagher@gmail.com>
2022-10-13 21:51:59 +0100

    Start python game

---
+++ python/game.py
@@ -0,0 +1,12 @@
+def welcome():
+    print("Who Wants To Be A Python Millionaire")
+    print(
+        """
+    HOW TO PLAY 
+    I am a process on your computer.
+    If you get any question wrong I will be killed
+    So get all the questions right...
+    """)
+
+
+welcome()
