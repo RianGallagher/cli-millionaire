d61455be4137940590ea5620b7d83a565ca0cc16
Rian Gallagher <rianogallagher@gmail.com>
2022-10-14 23:18:01 +0100

    Get basic game working with python

--- python/game.py
+++ python/game.py
@@ -1,3 +1,9 @@
+import inquirer
+import os
+
+player_name = ""
+
+
 def welcome():
     print("Who Wants To Be A Python Millionaire")
     print(
@@ -6,7 +12,59 @@ def welcome():
     I am a process on your computer.
     If you get any question wrong I will be killed
     So get all the questions right...
-    """)
+    """
+    )
 
 
 welcome()
+
+
+def ask_name():
+    # player_name: str = input("What is your name? (Player) ") or "Player"
+    # print(player_name)
+
+    answers = inquirer.prompt(
+        [
+            inquirer.Text(
+                "player_name", message="What is your name?", default="Player"
+            ),
+        ]
+    )
+    global player_name
+    player_name = answers["player_name"]
+
+
+ask_name()
+
+
+def handle_answer(isCorrect: bool):
+    print("Checking answer...")
+    if isCorrect:
+        print(f"Nice work { player_name }.")
+    else:
+        print(f"💀💀💀 Game over, you lose {player_name}!")
+
+
+def question_1():
+    correct = "1991"
+    answer = inquirer.prompt(
+        [
+            inquirer.List(
+                "answer",
+                message="Python was created by Guido van Rossum and released in what Year?",
+                choices=["1989", "1995", "2000", correct],
+            )
+        ]
+    )
+    handle_answer(answer["answer"] == correct)
+
+
+question_1()
+
+
+def winner():
+    os.system("clear")
+    print(f"Congrats {player_name}! \n $ 1 , 0 0 0 , 0 0 0")
+
+
+winner()
